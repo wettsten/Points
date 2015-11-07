1b8117751c6b280a1aa9a47b8456643cc33f7878
Scott Wettstein <scott.wettstein@gmail.com>
2015-11-07 12:22:18 -0600

    trying new approach to api

---
+++ src/Points.Api2/Controllers/CategoryController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Points.Data;
+
+namespace Points.Api2.Controllers
+{
+    [RoutePrefix("api/category")]
+    public class CategoryController : ApiController
+    {
+        [Authorize]
+        [Route("")]
+        public IHttpActionResult Get()
+        {
+            var categories = new List<Category>
+            {
+                new Category
+                {
+                    Id = 1,
+                    Name = "Housekeeping"
+                },
+                new Category
+                {
+                    Id = 2,
+                    Name = "Learning"
+                },
+                new Category
+                {
+                    Id = 3,
+                    Name = "Finance"
+                },
+                new Category
+                {
+                    Id = 4,
+                    Name = "Spiritual"
+                }
+            };
+            return Ok(categories);
+        }
+    }
+}
