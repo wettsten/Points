31ebaf3021bdeba6ed45c741d048cd6a2b8a7ae6
Scott Wettstein <scott.wettstein@gmail.com>
2015-12-09 09:41:40 -0600

    Added new row directive to categories

---
+++ src/Points.Web/app/directives/newCatRow.js
@@ -0,0 +1,38 @@
+﻿'use strict';
+app.directive('newCatRow', function() {
+    return {
+        scope: {},
+        templateUrl: '/app/views/newCatRow.html',
+        replace: true,
+        controller: 'newCatRowController'
+    };
+}).controller('newCatRowController', ['$scope', 'catsService', 'authService', function ($scope, catsService, authService) {
+
+    $scope.addCatData = {};
+
+    $scope.clearAddData = function () {
+        $scope.addCatData = {};
+    };
+
+    $scope.addCat = function () {
+        //if ($scope.addHasError('addName')) {
+        //    $scope.$parent.message = 'Name is required!';
+        //    return;
+        //}
+        $scope.addCatData.userId = authService.authentication.userId;
+        catsService.addCat($scope.addCatData).then(function (response) {
+            $scope.clearAddData();
+            $scope.$parent.loadCats();
+            },
+         function (err) {
+             $scope.$parent.message = err.data.message;
+         });
+    };
+
+    //$scope.addHasError = function (field, validation) {
+    //    if (validation) {
+    //        return $scope.addForm[field].$error[validation];
+    //    }
+    //    return $scope.addForm[field].$invalid;
+    //};
+}]);
