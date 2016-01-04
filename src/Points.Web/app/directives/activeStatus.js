877746a17ff8f9d393a9e040d333cea1be8e7d70
Scott Wettstein <scott.wettstein@gmail.com>
2016-01-03 21:57:07 -0600

    updated status bars to be closed initially and updated titles

--- src/Points.Web/app/directives/activeStatus.js
+++ src/Points.Web/app/directives/activeStatus.js
@@ -6,65 +6,89 @@ app.directive('activeStatus', function() {
         replace: true,
         controller: 'activeStatusController'
     };
-}).controller('activeStatusController', ['$scope', '$interval', 'jobsService', '$timeout', function ($scope, $interval, jobsService, $timeout) {
+}).controller('activeStatusController', ['$scope', '$interval', 'jobsService', '$timeout', '$q', 'activeTasksService', function ($scope, $interval, jobsService, $timeout, $q, activeTasksService) {
 
     $scope.render = true;
     $scope.screen = {
         left: 0,
         top: 50,
-        width: 250
+        width: 300
     };
     $scope.status = {
         title: 'Active Countdown',
-        isOpen: true,
+        isOpen: false,
         time: '',
         diff: '',
         color: 'panel-success'
     };
 
+    $scope.getEndJob = jobsService.getEndJob().then(
+        function (results) {
+            return results.data;
+        });
+
+    $scope.loadActiveTasks = activeTasksService.getTasks().then(
+        function (results) {
+            return results.data;
+        });
+
     $scope.loadData = function() {
-        jobsService.getEndJob().then(
-            function (result) {
-                var now = new Date();
-                var then = new Date(result.data.trigger);
-                var diff = then - now;
-
-                var days = Math.floor(diff / (1000 * 60 * 60 * 24));
-                diff -= days * (1000 * 60 * 60 * 24);
-
-                var hours = Math.floor(diff / (1000 * 60 * 60));
-                diff -= hours * (1000 * 60 * 60);
-
-                var mins = Math.floor(diff / (1000 * 60));
-
-                $scope.status.diff = days + ' days, ' + hours + ' hours, ' + mins + ' minutes';
-                $scope.status.time = then;
-                if (days < 1) {
-                    if ($scope.status.color !== 'panel-danger') {
-                        $scope.render = false;
-                        $scope.status.color = 'panel-danger';
-                        $timeout(function () {
-                            $scope.render = true;
-                        });
-                    }
-                } else if (days < 4) {
-                    if ($scope.status.color !== 'panel-warning') {
-                        $scope.render = false;
-                        $scope.status.color = 'panel-warning';
-                        $timeout(function () {
-                            $scope.render = true;
-                        });
-                    }
-                } else {
-                    if ($scope.status.color !== 'panel-success') {
-                        $scope.render = false;
-                        $scope.status.color = 'panel-success';
-                        $timeout(function () {
-                            $scope.render = true;
-                        });
-                    }
-                }
+        $q.all([$scope.getEndJob, $scope.loadActiveTasks]).then(
+            function (data) {
+                var days = $scope.calculateDate(data[0].trigger);
+                $scope.updatePanelColor(days);
+                var completed = data[1].filter(
+                    function (task) {
+                        return task.isCompleted;
+                    });
+                $scope.status.title = 'Active Countdown (' + completed.length + '/' + data[1].length + ')';
             });
+    };
+
+    $scope.calculateDate = function (trigger) {
+        var now = new Date();
+        var then = new Date(trigger);
+        var diff = then - now;
+
+        var days = Math.floor(diff / (1000 * 60 * 60 * 24));
+        diff -= days * (1000 * 60 * 60 * 24);
+
+        var hours = Math.floor(diff / (1000 * 60 * 60));
+        diff -= hours * (1000 * 60 * 60);
+
+        var mins = Math.floor(diff / (1000 * 60));
+
+        $scope.status.diff = days + ' days, ' + hours + ' hours, ' + mins + ' minutes';
+        $scope.status.time = then;
+        return days;
+    };
+
+    $scope.updatePanelColor = function (days) {
+        if (days < 1) {
+            if ($scope.status.color !== 'panel-danger') {
+                $scope.render = false;
+                $scope.status.color = 'panel-danger';
+                $timeout(function () {
+                    $scope.render = true;
+                });
+            }
+        } else if (days < 4) {
+            if ($scope.status.color !== 'panel-warning') {
+                $scope.render = false;
+                $scope.status.color = 'panel-warning';
+                $timeout(function () {
+                    $scope.render = true;
+                });
+            }
+        } else {
+            if ($scope.status.color !== 'panel-success') {
+                $scope.render = false;
+                $scope.status.color = 'panel-success';
+                $timeout(function () {
+                    $scope.render = true;
+                });
+            }
+        }
     };
 
     $scope.interval = $interval(function() {
--- src/Points.Web/app/directives/planningStatus.js
+++ src/Points.Web/app/directives/planningStatus.js
@@ -6,65 +6,85 @@ app.directive('planningStatus', function() {
         replace: true,
         controller: 'planningStatusController'
     };
-}).controller('planningStatusController', ['$scope', '$interval', 'jobsService', '$timeout', function($scope, $interval, jobsService, $timeout) {
+}).controller('planningStatusController', ['$scope', '$interval', 'jobsService', '$timeout', 'planningTasksService', '$q', function ($scope, $interval, jobsService, $timeout, planningTasksService, $q) {
 
     $scope.render = true;
     $scope.screen = {
-        left: window.innerWidth - 250,
+        left: window.innerWidth - 300,
         top: 50,
-        width: 250
+        width: 300
     };
     $scope.status = {
         title: 'Planning Countdown',
-        isOpen: true,
+        isOpen: false,
         time: '',
         diff: '',
         color: 'panel-success'
     };
 
+    $scope.getStartJob = jobsService.getStartJob().then(
+        function (results) {
+            return results.data;
+        });
+
+    $scope.loadPlanningTasks = planningTasksService.getTasks().then(
+        function (results) {
+            return results.data;
+        });
+
     $scope.loadData = function() {
-        jobsService.getStartJob().then(
-            function (result) {
-                var now = new Date();
-                var then = new Date(result.data.trigger);
-                var diff = then - now;
-
-                var days = Math.floor(diff / (1000 * 60 * 60 * 24));
-                diff -= days * (1000 * 60 * 60 * 24);
-
-                var hours = Math.floor(diff / (1000 * 60 * 60));
-                diff -= hours * (1000 * 60 * 60);
-
-                var mins = Math.floor(diff / (1000 * 60));
-
-                $scope.status.diff = days + ' days, ' + hours + ' hours, ' + mins + ' minutes';
-                $scope.status.time = then;
-                if (days < 1) {
-                    if ($scope.status.color !== 'panel-danger') {
-                        $scope.render = false;
-                        $scope.status.color = 'panel-danger';
-                        $timeout(function () {
-                            $scope.render = true;
-                        });
-                    }
-                } else if (days < 4) {
-                    if ($scope.status.color !== 'panel-warning') {
-                        $scope.render = false;
-                        $scope.status.color = 'panel-warning';
-                        $timeout(function () {
-                            $scope.render = true;
-                        });
-                    }
-                } else {
-                    if ($scope.status.color !== 'panel-success') {
-                        $scope.render = false;
-                        $scope.status.color = 'panel-success';
-                        $timeout(function () {
-                            $scope.render = true;
-                        });
-                    }
-                }
+        $q.all([$scope.getStartJob, $scope.loadPlanningTasks]).then(
+            function (data) {
+                var days = $scope.calculateDate(data[0].trigger);
+                $scope.updatePanelColor(days);
+                $scope.status.title = 'Planning Countdown (' + data[1].length + ')';
             });
+    };
+
+    $scope.calculateDate = function(trigger) {
+        var now = new Date();
+        var then = new Date(trigger);
+        var diff = then - now;
+
+        var days = Math.floor(diff / (1000 * 60 * 60 * 24));
+        diff -= days * (1000 * 60 * 60 * 24);
+
+        var hours = Math.floor(diff / (1000 * 60 * 60));
+        diff -= hours * (1000 * 60 * 60);
+
+        var mins = Math.floor(diff / (1000 * 60));
+
+        $scope.status.diff = days + ' days, ' + hours + ' hours, ' + mins + ' minutes';
+        $scope.status.time = then;
+        return days;
+    };
+
+    $scope.updatePanelColor = function(days) {
+        if (days < 1) {
+            if ($scope.status.color !== 'panel-danger') {
+                $scope.render = false;
+                $scope.status.color = 'panel-danger';
+                $timeout(function () {
+                    $scope.render = true;
+                });
+            }
+        } else if (days < 4) {
+            if ($scope.status.color !== 'panel-warning') {
+                $scope.render = false;
+                $scope.status.color = 'panel-warning';
+                $timeout(function () {
+                    $scope.render = true;
+                });
+            }
+        } else {
+            if ($scope.status.color !== 'panel-success') {
+                $scope.render = false;
+                $scope.status.color = 'panel-success';
+                $timeout(function () {
+                    $scope.render = true;
+                });
+            }
+        }
     };
 
     $scope.interval = $interval(function() {
