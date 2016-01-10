862ee2af2ff9cbfa5149bbfcab1a67a66a16e75e
Scott Wettstein <scott.wettstein@gmail.com>
2016-01-10 14:23:07 -0600

    Fixed job controller to return not found if job is not found

--- src/Points.Api.Resources/Controllers/JobsController.cs
+++ src/Points.Api.Resources/Controllers/JobsController.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Web.Http;
 using System.Web.Http.Results;
 using Points.Common.Processors;
-using Points.Data.EnumExtensions;
-using Points.Data.View;
 using Points.Scheduler.Jobs;
 using RavenJob = Points.Data.Raven.Job;
 using ViewJob = Points.Data.View.Job;
@@ -23,10 +19,14 @@ namespace Points.Api.Resources.Controllers
         public IHttpActionResult GetStartJobForUser()
         {
             var jobs = GetForUser();
-            if (jobs is OkNegotiatedContentResult<IOrderedEnumerable<Job>>)
+            if (jobs is OkNegotiatedContentResult<IOrderedEnumerable<ViewJob>>)
             {
-                var content = (jobs as OkNegotiatedContentResult<IOrderedEnumerable<Job>>).Content;
+                var content = (jobs as OkNegotiatedContentResult<IOrderedEnumerable<ViewJob>>).Content;
                 var job = content.FirstOrDefault(i => i.Processor.Equals(typeof (StartWeekJob).Name));
+                if (job == null)
+                {
+                    return NotFound();
+                }
                 return Ok(job);
             }
             return jobs;
@@ -36,10 +36,14 @@ namespace Points.Api.Resources.Controllers
         public IHttpActionResult GetEndJobForUser()
         {
             var jobs = GetForUser();
-            if (jobs is OkNegotiatedContentResult<IOrderedEnumerable<Job>>)
+            if (jobs is OkNegotiatedContentResult<IOrderedEnumerable<ViewJob>>)
             {
-                var content = (jobs as OkNegotiatedContentResult<IOrderedEnumerable<Job>>).Content;
+                var content = (jobs as OkNegotiatedContentResult<IOrderedEnumerable<ViewJob>>).Content;
                 var job = content.FirstOrDefault(i => i.Processor.Equals(typeof(EndWeekJob).Name));
+                if (job == null)
+                {
+                    return NotFound();
+                }
                 return Ok(job);
             }
             return jobs;
