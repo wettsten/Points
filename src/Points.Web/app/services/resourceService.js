0b593c67647398e3a5d99ea6062d4a79b6b572c8
Scott Wettstein <scott.wettstein@gmail.com>
2016-02-17 22:14:21 -0600

    Implemented caching in resource service

--- src/Points.Web/app/services/resourceService.js
+++ src/Points.Web/app/services/resourceService.js
@@ -1,9 +1,23 @@
 ﻿'use strict';
-app.factory('resourceService', ['$http', 'ngAuthSettings', '$timeout', function ($http, ngAuthSettings, $timeout) {
+app.factory('resourceService', ['$http', 'ngAuthSettings', '$timeout', '$cacheFactory', 'authService', function ($http, ngAuthSettings, $timeout, $cacheFactory, authService) {
 
     var serviceBase = ngAuthSettings.apiResourceBaseUri;
     var service = {};
     var callbacks = [];
+    var caches = {};
+
+    var getCache = function () {
+        if (!caches[authService.authentication.userId]) {
+            var cache = $cacheFactory(authService.authentication.userId);
+            caches[authService.authentication.userId] = cache;
+            return cache;
+        }
+        return caches[authService.authentication.userId];
+    };
+
+    var setCache = function (type, data) {
+        getCache().put(type, data);
+    };
 
     var callCallbacks = function(type,data) {
         for (var i = 0; i < callbacks.length; i++) {
@@ -17,32 +31,50 @@ app.factory('resourceService', ['$http', 'ngAuthSettings', '$timeout', function 
         callbacks.push({ type: type, callback: callback });
     };
 
-    service.get = function(type) {
-        $http.get(serviceBase + 'api/' + type).then(function(results) {
+    var retrieveWithRetry = function (type,retry) {
+        $http.get(serviceBase + 'api/' + type).then(function (results) {
+            if (angular.toJson(getCache().get(type)) === angular.toJson(results.data) && retry < 5) {
+                $timeout(retrieveWithRetry(type, ++retry), 250);
+                return;
+            }
+            setCache(type, results.data);
             callCallbacks(type, results.data);
         });
+    }
+
+    var retrieve = function (type) {
+        retrieveWithRetry(type, 0);
+    };
+
+    service.get = function (type) {
+        var cache = getCache().get(type);
+        if (!cache) {
+            retrieve(type);
+        } else {
+            $timeout(callCallbacks(type, cache), 10);
+        }
     };
 
     service.add = function (type,data) {
         return $http.post(serviceBase + 'api/' + type, data).then(function () {
-            $timeout(service.get(type), 1000);
+            $timeout(retrieve(type), 1000);
             if (type === 'planningtasks') {
-                $timeout(service.get('availabletasks'), 500);
+                $timeout(retrieve('availabletasks'), 500);
             }
         });
     };
 
     service.edit = function (type,data) {
         return $http.put(serviceBase + 'api/' + type, data).then(function () {
-            $timeout(service.get(type), 250);
+            $timeout(retrieve(type), 250);
         });
     };
 
     service.delete = function (type,id) {
         return $http.delete(serviceBase + 'api/' + type + '?id=' + id).then(function () {
-            $timeout(service.get(type), 250);
+            $timeout(retrieve(type), 250);
             if (type === 'planningtasks') {
-                $timeout(service.get('availabletasks'), 250);
+                $timeout(retrieve('availabletasks'), 250);
             }
         });
     };
