3bfd03b02f1b652a8c8906387762e844c6153829
Scott Wettstein <scott.wettstein@gmail.com>
2016-02-26 14:15:23 -0600

    Moved social login to its own directive

---
+++ src/Points.Web/app/directives/socialLogin.js
@@ -0,0 +1,9 @@
+﻿'use strict';
+app.directive('socialLogin', function() {
+    return {
+        scope: {
+        },
+        templateUrl: '/app/views/partials/socialLogin.html',
+        replace: true
+    };
+});
