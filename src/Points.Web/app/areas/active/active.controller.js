be0e3477228f8df7fdff89ab536d784134518f2e
Scott Wettstein <scott.wettstein@gmail.com>
2016-03-02 13:33:14 -0600

    Fixed active controller

--- src/Points.Web/app/areas/active/active.controller.js
+++ src/Points.Web/app/areas/active/active.controller.js
@@ -5,34 +5,30 @@
         .module('checkpoint')
         .controller('activeController', activeController);
 
-    activeController.$inject = ['$scope', 'resourceService', 'filterFactory', '$timeout'];
+    activeController.$inject = ['$scope', 'resourceService', 'filterService', '$timeout'];
 
-    function activeController($scope, resourceService, filterFactory, $timeout) {
+    function activeController($scope, resourceService, filterService, $timeout) {
         /* jshint validthis:true */
         var activeVm = this;
 
         activeVm.noItems = false;
         activeVm.tasks = [];
-        activeVm.taskFilter = filterFactory.getPTaskFilter();
+        activeVm.taskFilter = filterService.getPTaskFilter();
 
         activate();
 
         function activate() {
-            filterFactory.subscribe($scope, 'ptaskFilter', function () {
-                activeVm.taskFilter = filterFactory.getPTaskFilter();
+            filterService.subscribe($scope, 'ptaskFilter', function () {
+                activeVm.taskFilter = filterService.getPTaskFilter();
             });
 
-            $timeout(function () {
-                resourceService.get('activetasks', function (data) {
-                    activeVm.tasks = data;
-                    if (data.length === 0) {
-                        if (activeVm.addWarning) {
-                            activeVm.addWarning('No active tasks found');
-                        }
-                        activeVm.noItems = true;
-                    }
-                });
-            }, 100);
+            resourceService.get('activetasks', function (data) {
+                activeVm.tasks = data;
+                if (data.length === 0) {
+                    activeVm.addWarning('No active tasks found');
+                    activeVm.noItems = true;
+                }
+            });
         }
     }
 })();
